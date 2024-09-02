3d90d6d3a353a788d27c8e6ad3b199359c3e0343
MohamedRouini2 <medrouini95@outlook.com>
2024-09-02 05:27:46 +0200

    feat: Make parent dashboard accessible only by pin code
    fix "Beemo is typing" not appearing on dark mode, make the root page and /pageOff unaccessible when user is logged in.

--- blueprints/utils.py
+++ blueprints/utils.py
@@ -1,5 +1,7 @@
 from functools import wraps
-from flask import redirect, url_for, session
+from flask import redirect, url_for, session, redirect
+from datetime import datetime, timedelta
+
 
 def login_required(f):
     @wraps(f)
@@ -9,3 +11,18 @@ def login_required(f):
         return f(*args, **kwargs)
     return decorated_function
 
+def pin_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get('parent_zone_pin') != '1234':
+            return redirect(url_for('main.show_pin_modal'))
+        return f(*args, **kwargs)
+    return decorated_function
+
+def logged_in_restricted(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'user_id' in session:  # or any other condition to check if the user is logged in
+            return redirect(url_for('main.Home'))  # redirect to a different page or show an error
+        return f(*args, **kwargs)
+    return decorated_function
